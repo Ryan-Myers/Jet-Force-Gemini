bbb0a445ff6cc755870218ca2dc6a22107f87219
Ryan Myers <ryan.p.myers@gmail.com>
2023-03-22 10:38:17 -0400

    Rename vars in pi.c

--- src/pi.c
+++ src/pi.c
@@ -1,11 +1,11 @@
 #include "common.h"
 
-OSIoMesg D_800FECF0_FF8F0; //gAssetsDmaIoMesg;
-OSMesg D_800FED08_FF908; //gDmaMesg;
-OSMesgQueue D_800FED10_FF910; //gDmaMesgQueue;
-OSMesg D_800FED28_FF928[16]; //gPIMesgBuf[16];
-OSMesgQueue D_800FED68_FF968; //gPIMesgQueue
-u32 *D_800FED80_FF980; //gAssetsLookupTable
+OSIoMesg gAssetsDmaIoMesg;
+OSMesg gDmaMesg;
+OSMesgQueue gDmaMesgQueue;
+OSMesg gPIMesgBuf[16];
+OSMesgQueue gPIMesgQueue;
+u32 *gAssetsLookupTable;
 
 extern u8 __ASSETS_LUT_START[], __ASSETS_LUT_END[];
 
@@ -16,12 +16,12 @@ extern u8 __ASSETS_LUT_START[], __ASSETS_LUT_END[];
 */
 void piInit(void) {
     u32 assetTableSize;
-    osCreateMesgQueue(&D_800FED68_FF968, D_800FED28_FF928, ARRAY_COUNT(D_800FED28_FF928));
-    osCreateMesgQueue(&D_800FED10_FF910, &D_800FED08_FF908, 1);
-    osCreatePiManager((OSPri) 150, &D_800FED68_FF968, D_800FED28_FF928, ARRAY_COUNT(D_800FED28_FF928));
+    osCreateMesgQueue(&gPIMesgQueue, gPIMesgBuf, ARRAY_COUNT(gPIMesgBuf));
+    osCreateMesgQueue(&gDmaMesgQueue, &gDmaMesg, 1);
+    osCreatePiManager((OSPri) 150, &gPIMesgQueue, gPIMesgBuf, ARRAY_COUNT(gPIMesgBuf));
     assetTableSize = __ASSETS_LUT_END - __ASSETS_LUT_START;
-    D_800FED80_FF980 = (u32 *) mmAlloc(assetTableSize, COLOUR_TAG_GREY);
-    romCopy((u32) __ASSETS_LUT_START, (u32) D_800FED80_FF980, (s32) assetTableSize);
+    gAssetsLookupTable = (u32 *) mmAlloc(assetTableSize, COLOUR_TAG_GREY);
+    romCopy((u32) __ASSETS_LUT_START, (u32) gAssetsLookupTable, (s32) assetTableSize);
 }
 
 /**
@@ -32,11 +32,11 @@ u32 *piRomLoad(u32 assetIndex) {
     u32 *out;
     s32 size;
     u32 start;
-    if (D_800FED80_FF980[0] < assetIndex) {
+    if (gAssetsLookupTable[0] < assetIndex) {
         return 0;
     }
     assetIndex++;
-    index = assetIndex + D_800FED80_FF980;
+    index = assetIndex + gAssetsLookupTable;
     start = *index;
     size = *(index + 1) - start;
 	if (size == 0) {
@@ -60,11 +60,11 @@ UNUSED u8 *piRomLoadCompressed(u32 assetIndex, s32 extraMemory) {
     s32 totalSpace;
     u8 *gzipHeaderRamPos;
     u8 *out;
-    if (D_800FED80_FF980[0] < assetIndex) {
+    if (gAssetsLookupTable[0] < assetIndex) {
         return NULL;
     }
     assetIndex++;
-    out = (u8 *) (assetIndex + D_800FED80_FF980);
+    out = (u8 *) (assetIndex + gAssetsLookupTable);
     start = ((s32 *) out)[0];
     size = ((s32 *) out)[1] - start;
     gzipHeaderRamPos = (u8 *) mmAlloc(8, COLOUR_TAG_WHITE);
@@ -90,12 +90,12 @@ s32 piRomLoadSection(u32 assetIndex, u32 address, s32 assetOffset, s32 size) {
     u32 *index;
     s32 start;
 
-    if (size == 0 || D_800FED80_FF980[0] < assetIndex) {
+    if (size == 0 || gAssetsLookupTable[0] < assetIndex) {
         return 0;
     }
 
     assetIndex++;
-    index = assetIndex + D_800FED80_FF980;
+    index = assetIndex + gAssetsLookupTable;
     start = *index + assetOffset;
     romCopy((u32) (start + __ASSETS_LUT_END), address, size);
     return size;
@@ -108,12 +108,12 @@ u8 *piRomGetSectionPtr(u32 assetIndex, u32 assetOffset) {
     u32 *index;
     u32 start;
 
-    if (D_800FED80_FF980[0] < assetIndex) {
+    if (gAssetsLookupTable[0] < assetIndex) {
         return NULL;
     }
 
     assetIndex++;
-    index = assetIndex + D_800FED80_FF980;
+    index = assetIndex + gAssetsLookupTable;
     start = *index + assetOffset;
     return start + __ASSETS_LUT_END;
 }
@@ -124,12 +124,12 @@ u8 *piRomGetSectionPtr(u32 assetIndex, u32 assetOffset) {
 s32 piRomGetFileSize(u32 assetIndex) {
     u32 *index;
 
-    if (D_800FED80_FF980[0] < assetIndex) {
+    if (gAssetsLookupTable[0] < assetIndex) {
         return 0;
     }
 
     assetIndex++;
-    index = assetIndex + D_800FED80_FF980;
+    index = assetIndex + gAssetsLookupTable;
     return *(index + 1) - *index;
 }
 
@@ -148,8 +148,8 @@ void romCopy(u32 romOffset, u32 ramAddress, s32 numBytes) {
         if (numBytes < numBytesToDMA) {
             numBytesToDMA = numBytes;
         }
-        osPiStartDma(&D_800FECF0_FF8F0, OS_MESG_PRI_NORMAL, OS_READ, romOffset, (u32 *) ramAddress, numBytesToDMA, &D_800FED10_FF910);
-        osRecvMesg(&D_800FED10_FF910, &dmaMesg, OS_MESG_BLOCK);
+        osPiStartDma(&gAssetsDmaIoMesg, OS_MESG_PRI_NORMAL, OS_READ, romOffset, (u32 *) ramAddress, numBytesToDMA, &gDmaMesgQueue);
+        osRecvMesg(&gDmaMesgQueue, &dmaMesg, OS_MESG_BLOCK);
         numBytes -= numBytesToDMA;
         romOffset += numBytesToDMA;
         ramAddress += numBytesToDMA;
