cc4e44344e0c5daaf8c387fd7764ccfe5e305200
Ryan Myers <ryan.p.myers@gmail.com>
2023-05-10 14:25:39 -0400

    Test JPN build with some matches.

--- src_jpn/audiomgr.c
+++ src_jpn/audiomgr.c
@@ -8,7 +8,9 @@
 
 #pragma GLOBAL_ASM("asm_jpn/nonmatchings/audiomgr/amStop.s")
 
-#pragma GLOBAL_ASM("asm_jpn/nonmatchings/audiomgr/amAudioMgrGetNextFrameCount.s")
+s32 amAudioMgrGetNextFrameCount(void) {
+    return nextFrameCount;
+}
 
 #pragma GLOBAL_ASM("asm_jpn/nonmatchings/audiomgr/__amHandleFrameMsg.s")
 
