c0e67082aa37ebdd361f0eb42440b854ad7e63f5
Ryan Myers <ryan.p.myers@gmail.com>
2025-07-15 12:03:53 -0400

    Match acosf

--- src/acosf.c
+++ src/acosf.c
@@ -1,3 +1,18 @@
 #include "common.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/acosf/acosf.s")
+u16 acoss(s16);
+
+f32 acosf(f32 x) {
+    s16 signed_num;
+    u16 unsigned_num;
+
+    if (x >= 1.0f) {
+        signed_num = 0x7FFF;
+    } else if (x <= -1.0f) {
+        signed_num = -0x7FFF;
+    } else {
+        signed_num = x * 0x7FFF;
+    }
+    unsigned_num = acoss(signed_num);
+    return (unsigned_num * 3.141592741f) / 0xFFFF;
+}
