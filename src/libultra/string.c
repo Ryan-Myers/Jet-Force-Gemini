7bc22a84566c3a15b02afa364b5ec380f6115f1e
Ryan Myers <ryan.p.myers@gmail.com>
2025-07-15 22:29:28 -0400

    Fully match boot.c

--- src/libultra/string.c
+++ src/libultra/string.c
@@ -1,30 +1,30 @@
-#include "PR/ultratypes.h"
 #include "string.h"
+#include "PR/ultratypes.h"
 
 // TODO: this comes from a header
 #ident "$Revision: 1.23 $"
 
-void* memcpy(void* s1, const void* s2, size_t n) {
-    char* su1 = (char*)s1;
-    const char* su2 = (const char*)s2;
+void *memcpy(void *s1, const void *s2, size_t n) {
+    char *su1 = (char *) s1;
+    const char *su2 = (const char *) s2;
     while (n > 0) {
         *su1 = *su2;
         su1++;
         su2++;
         n--;
     }
-    return (void*)s1;
+    return (void *) s1;
 }
 
-size_t strlen(const char* s) {
-    const char* sc = s;
+size_t strlen(const char *s) {
+    const char *sc = s;
     while (*sc != 0) {
         sc++;
     }
     return sc - s;
 }
 
-char* strchr(const char* s, int c) {
+char *strchr(const char *s, int c) {
     const char ch = c;
     while (*s != ch) {
         if (*s == 0) {
@@ -32,5 +32,5 @@ char* strchr(const char* s, int c) {
         }
         s++;
     }
-    return (char*)s;
+    return (char *) s;
 }
