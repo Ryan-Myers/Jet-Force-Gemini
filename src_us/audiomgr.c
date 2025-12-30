20cd8a760fadbefd86cc6a61d415aefba186ae97
Ryan Myers <ryan.p.myers@gmail.com>
2025-12-30 15:40:23 -0500

    audiomgr kiosk to us

--- src_us/audiomgr.c
+++ src_us/audiomgr.c
@@ -1,29 +1,34 @@
 #include "common.h"
 
+const char D_800AAA80[] = "audio: ai out of samples\n";
+const char D_800AAA9C[] = "OH DEAR - No audio DMA buffers left\n";
+const char D_800AAAC4[] = "Dma not done\n";
+
 #pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/amCreateAudioMgr.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/func_80001D7C_297C.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/__amMain.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/amGo.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/amStop.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/amAudioMgrGetNextFrameCount.s")
+s32 amAudioMgrGetNextFrameCount(void) {
+    return nextFrameCount;
+}
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/func_80001EF8_2AF8.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/__amHandleFrameMsg.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/func_80002484_3084.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/__amHandleDoneMsg.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/func_800024C4_30C4.s")
+//__amDma
+#pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/func_80002764.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/__amDmaNew.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/func_800026CC_32CC.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/__clearAudioDMA.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/amAudioMgrSetScheduleMode.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/amAudioMgrHaveRunALargeFrame.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/amAudioMgrHaveFreedLargeBuffers.s")
-
-#pragma GLOBAL_ASM("asm_us/nonmatchings/audiomgr/D_800AA100_AAD00.s")
