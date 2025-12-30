96ee5313c687e59c27be990be14bbf54aeeedfd9
Ryan Myers <ryan.p.myers@gmail.com>
2025-12-30 16:22:19 -0500

    sync audio_manager_4c50

--- src_us/audio_manager_4C50.c
+++ src_us/audio_manager_4C50.c
@@ -1,5 +1,8 @@
 #include "common.h"
 
+const char D_800AAB00[] = "am: Unknown OSCILLATOR type %d\n";
+const char D_800AAB20[] = "am: Out of oscillator states\n";
+
 #pragma GLOBAL_ASM("asm_us/nonmatchings/audio_manager_4C50/amVibratoInit.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/audio_manager_4C50/amInitOsc.s")
@@ -8,6 +11,26 @@
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/audio_manager_4C50/amStopOsc.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/audio_manager_4C50/_depth2Cents.s")
+#if 0
+f32 _depth2Cents(u8 arg0) {
+    f32 var_f0;
+    f32 var_f2;
+    u32 temp_t8;
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/audio_manager_4C50/D_800AA180_AAD80.s")
+    var_f0 = 1.030992985f;
+    var_f2 = 1.0f;
+    if (arg0 != 0) {
+        do {
+            if (arg0 & 1) {
+                var_f2 *= var_f0;
+            }
+            var_f0 *= var_f0;
+            temp_t8 = (arg0 >> 1);
+            arg0 = temp_t8 & 0xFF;
+        } while (temp_t8 != 0);
+    }
+    return var_f2;
+}
+#else
+#pragma GLOBAL_ASM("asm_us/nonmatchings/audio_manager_4C50/_depth2Cents.s")
+#endif
