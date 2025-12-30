62ec538dbe367eba087127c174c814e77acf9d09
Ryan Myers <ryan.p.myers@gmail.com>
2025-12-30 10:53:48 -0500

    rename n_cspgetstate

---
+++ src_us/libultra/n_cspgetstate.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm_us/nonmatchings/libultra/n_cspgetstate/n_alCSPGetState.s")
+
+#pragma GLOBAL_ASM("asm_us/nonmatchings/libultra/n_cspgetstate/func_800899D8_8A5D8.s")
