f3b62bf63934c90eb51abc30a00293c923827b97
Ryan Myers <ryan.p.myers@gmail.com>
2025-12-31 15:22:00 -0500

    camlight

--- src_us/camlight.c
+++ src_us/camlight.c
@@ -12,12 +12,12 @@
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/camlight/camlightUpdate.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/camlight/func_80023FA4_24BA4.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/camlight/func_80023FB4.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/camlight/camlightVisibilityCheck.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/camlight/func_80024330_24F30.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/camlight/func_80024340.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/camlight/func_80024430_25030.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/camlight/func_80024440.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/camlight/camlightDraw.s")
