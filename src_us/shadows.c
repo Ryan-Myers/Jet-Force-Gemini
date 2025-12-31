8cea4c19980490ec6c4465f93f9de8c853df0e45
Ryan Myers <ryan.p.myers@gmail.com>
2025-12-31 15:11:08 -0500

    sync shadows

--- src_us/shadows.c
+++ src_us/shadows.c
@@ -10,16 +10,16 @@
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/shadows/shadowGenerate.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/shadows/func_8001E17C_1ED7C.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/shadows/func_8001DF5C.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/shadows/func_8001E8B8_1F4B8.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/shadows/func_8001E684.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/shadows/func_8001EE1C_1FA1C.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/shadows/func_8001EBE8.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/shadows/func_8001F4BC_200BC.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/shadows/func_8001F288.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/shadows/func_8001FA24_20624.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/shadows/func_8001F7C8.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/shadows/func_8001FFE0_20BE0.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/shadows/func_8001FD20.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/shadows/func_800203C4_20FC4.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/shadows/func_80020104.s")
