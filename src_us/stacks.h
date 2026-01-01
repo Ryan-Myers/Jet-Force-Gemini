0e75d235ebd33ce100efe38f3c740b1d9f9ab176
Ryan Myers <ryan.p.myers@gmail.com>
2026-01-01 09:58:31 -0500

    sync boot

---
+++ src_us/stacks.h
@@ -0,0 +1,17 @@
+#ifndef _STACKS_H_
+#define _STACKS_H_
+
+#include "types.h"
+
+#define STACK_EPC       0x1000
+#define STACK_EPCINFO1  0x200
+#define STACK_EPCINFO2  0x400
+#define STACK_IDLE      0x0 // Zero sized stack? Why!?
+#define STACK_SCHED     0x2000
+#define STACK_AUD       0x3010
+#define STACK_GAME      0x2000
+#define STACK_BGLOAD    0x2000
+
+#define STACKSIZE(x) (x / sizeof(u64))
+
+#endif
