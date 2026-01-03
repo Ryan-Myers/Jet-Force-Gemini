b3b75b8c979ec9ba6ccd65157f95f3ef58b3afcf
Ryan Myers <ryan.p.myers@gmail.com>
2026-01-03 13:34:17 -0500

    update sched func names

--- src_us/sched.c
+++ src_us/sched.c
@@ -14,30 +14,30 @@
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/sched/__scMain.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/func_8004FB30_50730.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/func_80050670.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/get_task_string.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/osScGetTaskType.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/func_8004FC64_50864.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/func_800507A4.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/func_8004FC6C_5086C.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/func_800507AC.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/func_8004FF64_50B64.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/func_80050AA4.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/func_80050154_50D54.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/__scHandleRetrace.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/func_800507F8_513F8.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/__scHandleRSP.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/sched/__scHandleRDP.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/sched/__scTaskReady.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/func_80050A08_51608.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/__scTaskComplete.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/sched/__scAppendList.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/sched/__scExec.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/func_80050C3C_5183C.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/__scYield.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/func_80050C90_51890.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/sched/__scSchedule.s")
