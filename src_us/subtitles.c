d50518c80c2813767f51d3c940e82f9a50ad1783
Ryan Myers <ryan.p.myers@gmail.com>
2026-01-04 16:30:47 -0500

    sync subtitles

--- src_us/subtitles.c
+++ src_us/subtitles.c
@@ -6,9 +6,9 @@
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/subtitles/subtitlesEnable.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/subtitles/func_800737B8_743B8.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/subtitles/func_80073F38.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/subtitles/func_80073A2C_7462C.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/subtitles/func_800741AC.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/subtitles/subtitlesTick.s")
 
