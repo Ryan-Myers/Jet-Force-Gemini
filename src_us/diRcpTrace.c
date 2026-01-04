1ea7b84a2938bd304cbc00c8e735beb977e1a5bd
Ryan Myers <ryan.p.myers@gmail.com>
2026-01-04 11:14:45 -0500

    sync diRcpTrace

--- src_us/diRcpTrace.c
+++ src_us/diRcpTrace.c
@@ -4,8 +4,9 @@
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/diRcpTrace/diRcpTraceReset.s")
 
+const char D_800AE670[] = "diRcpTrace: Buffer not allocated!\n";
+const char D_800AE694[] = "diRcpTrace: Buffer overflow!\n";
+
 #pragma GLOBAL_ASM("asm_us/nonmatchings/diRcpTrace/diRcpTrace.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/diRcpTrace/diRcpTraceGetInfo.s")
-
-#pragma GLOBAL_ASM("asm_us/nonmatchings/diRcpTrace/D_800ADAC0_AE6C0.s")
