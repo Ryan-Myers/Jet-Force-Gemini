a0933c324e305826b58248aacfd6cdd9008eed4e
Ryan Myers <ryan.p.myers@gmail.com>
2026-01-04 16:12:48 -0500

    sync fx

--- src_us/fx.c
+++ src_us/fx.c
@@ -10,11 +10,11 @@
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/fxLevelAllocateLevelEffects.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_800689B4_695B4.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_80068CD4.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_800689BC_695BC.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_80068CDC.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_80068A14_69614.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_80068D34.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/fxUpdateLevelEffects.s")
 
@@ -24,9 +24,9 @@
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/fxAllocateCone.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_80069000_69C00.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_80069320.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_800691F0_69DF0.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_80069510.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/fxMakeConeTextureCoords.s")
 
@@ -34,21 +34,21 @@
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/fxDrawCone.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_80069F04_6AB04.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006A224.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006A0C0_6ACC0.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/wakeAllocate.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/wakeSetupRipple.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006A894_6B494.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/wakeFree.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/wakeFreeRipple.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006A924_6B524.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/wakeUpdate.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/wakeUpdateRipple.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006B3CC_6BFCC.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/wakeDraw.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/wakeDrawRipple.s")
 
@@ -66,33 +66,35 @@
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/drawClearScreen.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006BE88_6CA88.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006C1B8.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006BE9C_6CA9C.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006C1CC.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006BF50_6CB50.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006C280.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006C124_6CD24.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006C454.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006C230_6CE30.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006C560.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006C77C_6D37C.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006CAAC.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006CA30_6D630.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006CD60.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006CB28_6D728.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006CE58.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006CC20_6D820.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006CF50.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006CD18_6D918.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006D048.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006CE78_6DA78.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006D1A8.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006CFC0_6DBC0.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006D2F0.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/fxInitLines.s")
 
+#ifdef VERSION_us
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006D110_6DD10.s")
+#endif
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/fxDrawLine.s")
 
@@ -104,17 +106,21 @@
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/fxInttostr.s")
 
+#ifdef VERSION_kiosk
+#pragma GLOBAL_ASM("asm/nonmatchings/fx/ClipPoint.s")
+#endif
+
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/fxDrawLine3D.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/PlotAddRGB.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/PlotAddRG.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006DB6C_6E76C.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006E1D8.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006DD20_6E920.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006E38C.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006DF90_6EB90.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006E5FC.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/fxDrawDigitalNumber.s")
 
@@ -138,7 +144,7 @@
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/fxScreenEffect.s")
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006F874_70474.s")
+#pragma GLOBAL_ASM("asm_us/nonmatchings/fx/func_8006FFF8.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/fx/fxGenerateTextures.s")
 
