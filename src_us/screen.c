80372e4e072e19a63b2fb5c45254de3d6898b786
Ryan Myers <ryan.p.myers@gmail.com>
2026-01-03 21:45:54 -0500

    sync screen

--- src_us/screen.c
+++ src_us/screen.c
@@ -1,7 +1,7 @@
 #include "common.h"
 
+const char D_800ADF00[] = "SCREEN: No out of range!!\n";
+
 #pragma GLOBAL_ASM("asm_us/nonmatchings/screen/screenLoad.s")
 
 #pragma GLOBAL_ASM("asm_us/nonmatchings/screen/screenDraw.s")
-
-#pragma GLOBAL_ASM("asm_us/nonmatchings/screen/D_800AD350_ADF50.s")
