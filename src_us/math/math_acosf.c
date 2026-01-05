cafb6648ca9e3fe4ab50c6f7d66e10e182974aa6
Ryan Myers <ryan.p.myers@gmail.com>
2026-01-04 20:51:22 -0500

    sync math files

--- src_us/math/math_acosf.c
+++ src_us/math/math_acosf.c
@@ -1,3 +1,20 @@
-#include "common.h"
+#include "math.h"
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/math/math_acosf/acosf.s")
+/**
+ * Official Name: acosf
+ */
+f32 acosf(f32 value) {
+    s16 intval;
+    u16 uintval;
+
+    if (value >= 1) {
+        intval = 0x7FFF;
+    } else if (value <= -1) {
+        intval = -0x7FFF;
+    } else {
+        intval = value * 0x7FFF;
+    }
+
+    uintval = acoss(intval);
+    return (uintval * MATH_PI) / 0xFFFF;
+}
--- src_us/math/math_atan.c
+++ src_us/math/math_atan.c
@@ -1,3 +1,45 @@
 #include "common.h"
+#include "math.h"
 
-#pragma GLOBAL_ASM("asm_us/nonmatchings/math/math_atan/atan2f.s")
+/**
+ * Official Name: atan2f
+ */
+f32 atan2f(f32 x, f32 z) {
+    f32 result = 0;
+
+    if (x == 0) {
+        if (z >= 0) {
+            result = 0;
+        } else {
+            result = DTOR(180);
+        }
+    } else if (z == 0) {
+        if (x > 0) {
+            result = DTOR(90);
+        } else {
+            result = DTOR(270);
+        }
+    } else {
+        f32 sqrt = sqrtf(x * x + z * z);
+
+        if (x > z) {
+            result = acosf(z / sqrt);
+
+            if (x < 0) {
+                result = DTOR(360) - result;
+            }
+        } else {
+            result = DTOR(90) - acosf(x / sqrt);
+
+            if (z < 0) {
+                result = DTOR(180) - result;
+            }
+
+            if (result < 0) {
+                result += DTOR(360);
+            }
+        }
+    }
+
+    return result;
+}
