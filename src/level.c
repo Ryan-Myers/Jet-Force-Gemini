d618f7e208eb7e9654f9060eb7fd5a8990a1ef7e
queueRAM <queueRAM@proton.me>
2026-07-29 18:08:23 +0000

    Match levelNGetType
    Co-authored-by: inspectredc <78732756+inspectredc@users.noreply.github.com>

--- src/level.c
+++ src/level.c
@@ -6,9 +6,17 @@ const char D_800ACD20[] = "LOADLEVEL Error: Level out of range\n";
 const char D_800ACD48[] = "levelGetRegionFlags: Ran out of levelRegionFlag structures!!\n";
 const char D_800ACD88[] = "levelGetObjectID - Out of level flags\n";
 
+extern s32 D_800FB124_B1764;
+extern u8* D_800FB12C_B176C[];
+
 #pragma GLOBAL_ASM("asm/nonmatchings/level/levelGetCounts.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/level/levelNGetType.s")
+s32 levelNGetType(s32 arg0) {
+    if ((arg0 >= 0) && (arg0 < D_800FB124_B1764)) {
+        return D_800FB12C_B176C[0][arg0 * 5];
+    }
+    return -1;
+}
 
 #ifdef VERSION_us
 #pragma GLOBAL_ASM("asm/nonmatchings/level/levelGetTune.s")
