125457b83dbffc6f4942108c2399b969d4cd2069
Bl00D4NGEL <github@d-peters.com>
2026-08-15 21:16:53 +0200

    add missing headers

---
+++ include/overlays/overlay1.h
@@ -0,0 +1,9 @@
+#ifndef _OVERLAYS_OVERLAY1_H_
+#define _OVERLAYS_OVERLAY1_H_
+
+#include <PR/ultratypes.h>
+
+void bloodSpurtUpdateAll_Trap(s32 arg0);
+void dropletUpdateAll_Trap(s32 arg0);
+
+#endif
---
+++ src/weather.h
@@ -0,0 +1,15 @@
+#ifndef _WEATHER_H_
+#define _WEATHER_H_
+
+#include <PR/ultratypes.h>
+
+void initWeather(void);
+void setWeatherLimits(s16 near, s16 far);
+void freeWeather(void);
+void setupWeather(s32 arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s32 arg6);
+void changeWeather(s32 arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5);
+void doWeather(Gfx **currDisplayList, Mtx **currHudMat, Vertex **currHudVerts, Triangle **currHudTris, s32 updateRate);
+void rainSetFog(void);
+f32 rainDensity(void);
+
+#endif
