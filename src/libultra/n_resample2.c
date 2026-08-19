2c19c735011853c692d3c07846210ac13a573294
Ryan Myers <ryan.p.myers@gmail.com>
2026-08-19 14:58:20 -0400

    Rename n_alLPFilter to n_resample2 like in PD

---
+++ src/libultra/n_resample2.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/n_resample2/n_alLPFilterPull.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/n_resample2/n_alLPFilterParam.s")
