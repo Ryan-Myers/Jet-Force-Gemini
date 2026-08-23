6d057454629b7ca0ab0848bd32da744735de1884
Ryan Myers <ryan.p.myers@gmail.com>
2026-08-22 21:52:11 -0400

    More files matched

--- src/libultra/n_synaddplayer.c
+++ src/libultra/n_synaddplayer.c
@@ -1,7 +1,63 @@
-#include "common.h"
+/*====================================================================
+ *
+ * Copyright 1993, Silicon Graphics, Inc.
+ * All Rights Reserved.
+ *
+ * This is UNPUBLISHED PROPRIETARY SOURCE CODE of Silicon Graphics,
+ * Inc.; the contents of this file may not be disclosed to third
+ * parties, copied or duplicated in any form, in whole or in part,
+ * without the prior written permission of Silicon Graphics, Inc.
+ *
+ * RESTRICTED RIGHTS LEGEND:
+ * Use, duplication or disclosure by the Government is subject to
+ * restrictions as set forth in subdivision (c)(1)(ii) of the Rights
+ * in Technical Data and Computer Software clause at DFARS
+ * 252.227-7013, and/or in similar or successor clauses in the FAR,
+ * DOD or NASA FAR Supplement. Unpublished - rights reserved under the
+ * Copyright Laws of the United States.
+ *====================================================================*/
+#include <os_internal.h>
+#include "n_synthInternals.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/n_synaddplayer/n_alSynAddPlayer.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/n_synaddplayer/n_alSynAddSndPlayer.s")
+void n_alSynAddPlayer( ALPlayer *client)
+{
+    OSIntMask mask = osSetIntMask(OS_IM_NONE);
 
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/n_synaddplayer/n_alSynAddSeqPlayer.s")
+    client->samplesLeft = n_syn->curSamples;
+
+    client->next = n_syn->head;
+    n_syn->head   = client;
+
+    osSetIntMask(mask);
+}
+
+
+void n_alSynAddSndPlayer( ALPlayer *client)
+{
+    OSIntMask mask = osSetIntMask(OS_IM_NONE);
+
+    client->samplesLeft = n_syn->curSamples;
+
+#if 1
+    client->next = n_syn->head;
+    n_syn->head   = client;
+#endif
+
+    osSetIntMask(mask);
+}
+
+void n_alSynAddSeqPlayer( ALPlayer *client)
+{
+    OSIntMask mask = osSetIntMask(OS_IM_NONE);
+
+    client->samplesLeft = n_syn->curSamples;
+
+#if 1
+    client->next = n_syn->head;
+    n_syn->head   = client;
+#endif
+
+    osSetIntMask(mask);
+}
+
