47bee60728f1e7174de707564271a67785ab62cc
Ryan Myers <ryan.p.myers@gmail.com>
2026-08-26 07:45:23 -0400

    Cleanups

--- src/osBootRamTest.c
+++ src/osBootRamTest.c
@@ -1,13 +1,14 @@
 #include "osBootRamTest.h"
 #include "common.h"
 
-#define RAMTEST1_ADDRESS 0x2FB1F4 // 0xA02FB1F4
+#define RAMTEST1_ADDRESS 0x2FB1F4   // 0xA02FB1F4
 #define RAMTEST1_EXPECTED 0xD090010 // 0xAD090010
-#define RAMTEST2_ADDRESS 0x2FE1C0 // 0xA02FE1C0
+#define RAMTEST2_ADDRESS 0x2FE1C0   // 0xA02FE1C0
 #define RAMTEST2_EXPECTED 0xD170014 // 0xAD170014
 
 #ifdef VERSION_kiosk
-// First value is "TRUE" if the test has not been run yet, and "FALSE" if it has. Second value is the result of the test.
+// First value is "TRUE" if the test has not been run yet, and "FALSE" if it has. 
+// Second value is the result of the test.
 static s32 sRamTest1[2] = { TRUE, FALSE };
 static s32 sRamTest2[2] = { TRUE, FALSE };
 
@@ -16,6 +17,16 @@ s32 osBootRamTest1_6105(void) {
     if (ramTest[0] != FALSE) {
         ramTest[0] = FALSE;
         if (IO_READ(RAMTEST1_ADDRESS) == PHYS_TO_K1(RAMTEST1_EXPECTED)) {
+            ramTest[1] = TRUE;
+        }
+    }
+    return ramTest[1];
+}
+s32 osBootRamTest2_6105(void) {
+    s32 *ramTest = sRamTest2;
+    if (ramTest[0] != FALSE) {
+        ramTest[0] = FALSE;
+        if (IO_READ(RAMTEST2_ADDRESS) == PHYS_TO_K1(RAMTEST2_EXPECTED)) {
             ramTest[1] = TRUE;
         }
     }
@@ -32,20 +43,7 @@ s32 osBootRamTest1_6105(void) {
     IO_WRITE(RAMTEST1_ADDRESS, FALSE);
     return ret;
 }
-#endif
 
-#ifdef VERSION_kiosk
-s32 osBootRamTest2_6105(void) {
-    s32 *ramTest = sRamTest2;
-    if (ramTest[0] != FALSE) {
-        ramTest[0] = FALSE;
-        if (IO_READ(RAMTEST2_ADDRESS) == PHYS_TO_K1(RAMTEST2_EXPECTED)) {
-            ramTest[1] = TRUE;
-        }
-    }
-    return ramTest[1];
-}
-#else
 s32 osBootRamTest2_6105(void) {
     s32 ret;
 
